09359a49fd6df686ee387cc7d0b3352bd43f519f
Mario Torres <mariotee10@gmail.com>
2019-01-30 14:50:27 -0600

    Updates

---
+++ precache-manifest.db515bcf3b445113bd8055f56d13c1eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe11e84c4ae9cfe68d3c",
+    "url": "/Enigma-Machine-Web/static/js/runtime~main.fe11e84c.js"
+  },
+  {
+    "revision": "0510d6d96dc9f2ada270",
+    "url": "/Enigma-Machine-Web/static/js/main.0510d6d9.chunk.js"
+  },
+  {
+    "revision": "01d315b15fc01d4818fc",
+    "url": "/Enigma-Machine-Web/static/js/1.01d315b1.chunk.js"
+  },
+  {
+    "revision": "0510d6d96dc9f2ada270",
+    "url": "/Enigma-Machine-Web/static/css/main.c7f3c6ee.chunk.css"
+  },
+  {
+    "revision": "f85e9966562ab4b5ee948258212f2588",
+    "url": "/Enigma-Machine-Web/index.html"
+  }
+];
