373542c70a600dd263b8c15102639521dc583fd8
Mario Torres <mariotee10@gmail.com>
2019-02-01 13:39:05 -0600

    Updates

--- precache-manifest.16e505893892d6a0dee6f431a796821a.js
+++ precache-manifest.16e505893892d6a0dee6f431a796821a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Enigma-Machine-Web/static/js/runtime~main.fe11e84c.js"
   },
   {
-    "revision": "327e4ffa8f84b2e21c98",
-    "url": "/Enigma-Machine-Web/static/js/main.327e4ffa.chunk.js"
+    "revision": "1d00de921b66d40ba593",
+    "url": "/Enigma-Machine-Web/static/js/main.1d00de92.chunk.js"
   },
   {
     "revision": "b3de899eda14e164c393",
     "url": "/Enigma-Machine-Web/static/js/1.b3de899e.chunk.js"
   },
   {
-    "revision": "327e4ffa8f84b2e21c98",
-    "url": "/Enigma-Machine-Web/static/css/main.10897c4a.chunk.css"
+    "revision": "1d00de921b66d40ba593",
+    "url": "/Enigma-Machine-Web/static/css/main.54eb327e.chunk.css"
   },
   {
-    "revision": "d1a7202b0c52ca5c413a26378571aaca",
+    "revision": "3e4b1edb158302d0994a5d019fc58c06",
     "url": "/Enigma-Machine-Web/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Enigma-Machine-Web/precache-manifest.a9a6bdecdb5992c533576b8a4fe31116.js"
+  "/Enigma-Machine-Web/precache-manifest.16e505893892d6a0dee6f431a796821a.js"
 );
 
 workbox.clientsClaim();
